9058ca03daa3cdac9aa9265a5809edb741096962
Andrea Jasper <andrea.nicole.jasper@gmail.com>
2021-05-18 15:11:39 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -1,7 +1,48 @@
 const chai = require('chai')
- 
+const supertest = require('supertest')
+/* ADD ME! */
+const app = require('../app')
+
 const expect = chai.expect
- 
+
+let server
+
+before(function(done) {
+  return app.up().then(_server => {
+    server = _server
+    done()
+  })
+})
+
+after(function() {
+  server.close()
+})
+
 describe('app', function() {
-  it('runs')
+  describe('up', function() {
+    it('is a function', function() {
+      expect(app.up).to.be.an.instanceof(Function)
+    })
+  })
+  describe('/user', function() {
+    describe('POST', function() {
+      it('fails with an empty request body', function(done) {
+        supertest(server).
+          post('/user').
+          expect(400, done)
+        })
+
+        it('succeeds with valid name, username, and email', function(done) {
+        supertest(server).
+          post('/user').
+          send({
+            email: 'test@email.com',
+            name: 'testName',
+            username: 'testUsername'
+          }).
+          set('content-type', 'application/json').
+          expect(200, done)
+      })
+    })
+  })
 })
